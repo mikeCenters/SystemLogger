086fd2bdbaaa24dd9d93ef291a9fc497c0c51e8f
Mike Centers <michael.centers@icloud.com>
2024-09-23 22:14:50 -0400

    Added names to tests.

--- Tests/SystemLoggerTests/SystemLoggerTests.swift
+++ Tests/SystemLoggerTests/SystemLoggerTests.swift
@@ -9,37 +9,43 @@ let logger = SystemLogger(subsystem: "com.SystemLogger.Tests", category: "System
 // MARK: - Tests
 // Since os.log doesn't return values, manually verify the logs in Xcode Console
 
-@Test func testLogInfo() {
+@Test("Test information logging")
+func testLogInfo() {
     let message = "This is an informational message."
     logger.logInfo(message)
     // Check the log in the console
 }
 
-@Test func testLogDebug() {
+@Test("Test dubug logging")
+func testLogDebug() {
     let message = "Debugging process started."
     logger.logDebug(message)
     // Check the log in the console
 }
 
-@Test func testLogWarning() {
+@Test("Test warning message logging")
+func testLogWarning() {
     let message = "This is a warning message."
     logger.logWarning(message)
     // Check the log in the console
 }
 
-@Test func testLogError() {
+@Test("Test error logging")
+func testLogError() {
     let message = "An error occurred while loading data."
     logger.logError(message)
     // Check the log in the console
 }
 
-@Test func testLogCritical() {
+@Test("Test crital error logging")
+func testLogCritical() {
     let message = "Critical failure due to an unhandled exception."
     logger.logCritical(message)
     // Check the log in the console
 }
 
-@Test func testLogPrivate() {
+@Test("Test private logging")
+func testLogPrivate() {
     let sensitiveMessage = "User email: user@example.com"
     logger.logPrivate(sensitiveMessage)
     // Check the log in the console
